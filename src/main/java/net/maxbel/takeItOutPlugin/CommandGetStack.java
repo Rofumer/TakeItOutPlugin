8ef57180f15cf351d57aeba417021f3221e70a4f
Rofumer <rofumer@gmail.com>
2024-09-04 22:42:24 +0300

    Beta commit

--- src/main/java/net/maxbel/takeItOutPlugin/CommandGetStack.java
+++ src/main/java/net/maxbel/takeItOutPlugin/CommandGetStack.java
@@ -15,46 +15,26 @@ public class CommandGetStack implements PluginMessageListener {
     @Override
     public void onPluginMessageReceived(String channel, Player player, byte[] message) {
         ByteArrayDataInput buf = ByteStreams.newDataInput(message);
-
-
         int slot = buf.readInt();
         int inventory = buf.readInt();
-
-        //int slot = buf.readInt();
-        ItemStack shulker = player.getInventory().getContents()[inventory];
-
-        if (shulker == null || shulker.getType() == Material.AIR) {
-            return;
-        }
-        //if (player.getInventory().getItemInMainHand() != null
-        //        && player.getInventory().getItemInMainHand().getType() != Material.AIR)
-        //&& player.getInventory().getItemInMainHand().getItemMeta() instanceof BlockStateMeta bsm
-        //        && bsm.getBlockState() instanceof ShulkerBox)
-        //{
-            // Trying to put a shulker into a shulker, prevent this.
-         //   return;
-        //}
-        ItemMeta meta = shulker.getItemMeta();
-
-        if (meta instanceof BlockStateMeta bsm && bsm.getBlockState() instanceof ShulkerBox box) {
-            ItemStack tmp = box.getInventory().getItem(slot);
-
-            if(tmp == null || tmp.getType() == Material.AIR) { return;}
-
-            player.sendMessage(tmp.toString());
-
-            //box.getInventory().setItem(slot, player.getInventory().getItemInMainHand());
-            box.getInventory().setItem(slot, new ItemStack(Material.AIR));
-            //box.getInventory().remove(tmp); //setItem(slot, player.getInventory().getItemInMainHand());
-            //player.getInventory().setItemInMainHand(tmp);
-            int emptySlot=player.getInventory().firstEmpty();
-            player.getInventory().setItem(emptySlot,tmp);// setItemInMainHand(tmp);
-            ItemStack stackInMainHand=player.getInventory().getItemInMainHand();
-            player.getInventory().setItemInMainHand(tmp);
-            player.getInventory().setItem(emptySlot,stackInMainHand);
-            bsm.setBlockState(box);
-            shulker.setItemMeta(meta);
+        if (player.getInventory().firstEmpty() != -1) {
+            ItemStack shulker = player.getInventory().getContents()[inventory];
+            if (shulker == null || shulker.getType() == Material.AIR) {
+                return;
+            }
+            ItemMeta meta = shulker.getItemMeta();
+            if (meta instanceof BlockStateMeta bsm && bsm.getBlockState() instanceof ShulkerBox box) {
+                ItemStack tmp = box.getInventory().getItem(slot);
+                if (tmp == null || tmp.getType() == Material.AIR) {
+                    return;
+                }
+                //player.sendMessage(tmp.toString());
+                box.getInventory().setItem(slot, new ItemStack(Material.AIR));
+                player.getInventory().setItem(player.getInventory().firstEmpty(), player.getInventory().getItemInMainHand());
+                player.getInventory().setItemInMainHand(tmp);
+                bsm.setBlockState(box);
+                shulker.setItemMeta(meta);
+            }
         }
     }
-
 }
