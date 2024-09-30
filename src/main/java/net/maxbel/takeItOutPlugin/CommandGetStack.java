944e45aad1cddf67bfb06a44ac2b8f705bc36e4c
Rofumer <rofumer@gmail.com>
2024-09-30 12:55:41 +0300

    Next features has been added: 1) items swapping when inventory is full 2) blocks are picked if you right click on it in the schematic (trying the place it down)

--- src/main/java/net/maxbel/takeItOutPlugin/CommandGetStack.java
+++ src/main/java/net/maxbel/takeItOutPlugin/CommandGetStack.java
@@ -17,12 +17,14 @@ public class CommandGetStack implements PluginMessageListener {
         ByteArrayDataInput buf = ByteStreams.newDataInput(message);
         int slot = buf.readInt();
         int inventory = buf.readInt();
+        ItemStack shulker;
+        ItemMeta meta;
         if (player.getInventory().firstEmpty() != -1) {
-            ItemStack shulker = player.getInventory().getContents()[inventory];
+            shulker = player.getInventory().getContents()[inventory];
             if (shulker == null || shulker.getType() == Material.AIR) {
                 return;
             }
-            ItemMeta meta = shulker.getItemMeta();
+            meta = shulker.getItemMeta();
             if (meta instanceof BlockStateMeta bsm && bsm.getBlockState() instanceof ShulkerBox box) {
                 ItemStack tmp = box.getInventory().getItem(slot);
                 if (tmp == null || tmp.getType() == Material.AIR) {
@@ -36,5 +38,71 @@ public class CommandGetStack implements PluginMessageListener {
                 shulker.setItemMeta(meta);
             }
         }
+        else {
+            shulker = player.getInventory().getContents()[inventory];
+            if (shulker == null || shulker.getType() == Material.AIR) {
+                return;
+            }
+            meta = shulker.getItemMeta();
+            if (meta instanceof BlockStateMeta bsm && bsm.getBlockState() instanceof ShulkerBox box) {
+                ItemStack item;
+                for (int i = Math.min(36, player.getInventory().getSize()) - 1; i >= 0; --i) {
+                    item = player.getInventory().getItem(i);
+                    ItemMeta itemMeta = item.getItemMeta();
+                    if (!isTool(item) && !(itemMeta instanceof BlockStateMeta ibsm && ibsm.getBlockState() instanceof ShulkerBox ibox)) {
+                        ItemStack tmp = box.getInventory().getItem(slot);
+                        if (tmp == null || tmp.getType() == Material.AIR) {
+                            return;
+                        }
+                        //player.sendMessage(tmp.toString());
+                        box.getInventory().setItem(slot, item);
+                        player.getInventory().setItem(i, player.getInventory().getItemInMainHand());
+                        player.getInventory().setItemInMainHand(tmp);
+                        bsm.setBlockState(box);
+                        shulker.setItemMeta(meta);
+                    }
+                }
+            }
+        }
     }
+
+    public static boolean isTool(ItemStack item) {
+        if (item == null || item.getType() == Material.AIR) {
+            return false;
+        }
+
+        Material material = item.getType();
+
+        // Проверяем типы материалов, относящихся к инструментам
+        switch (material) {
+            case WOODEN_PICKAXE:
+            case STONE_PICKAXE:
+            case IRON_PICKAXE:
+            case GOLDEN_PICKAXE:
+            case DIAMOND_PICKAXE:
+            case NETHERITE_PICKAXE:
+            case WOODEN_AXE:
+            case STONE_AXE:
+            case IRON_AXE:
+            case GOLDEN_AXE:
+            case DIAMOND_AXE:
+            case NETHERITE_AXE:
+            case WOODEN_SHOVEL:
+            case STONE_SHOVEL:
+            case IRON_SHOVEL:
+            case GOLDEN_SHOVEL:
+            case DIAMOND_SHOVEL:
+            case NETHERITE_SHOVEL:
+            case WOODEN_HOE:
+            case STONE_HOE:
+            case IRON_HOE:
+            case GOLDEN_HOE:
+            case DIAMOND_HOE:
+            case NETHERITE_HOE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
 }
